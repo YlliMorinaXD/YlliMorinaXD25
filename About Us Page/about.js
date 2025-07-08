65812913df19ada2dd3251fd9d5b64c6dc0327d0
YlliMorinaXD <yllimorina579@gmail.com>
2025-07-08 13:20:57 +0200

    4

---
+++ About Us Page/about.js
@@ -0,0 +1,4 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    var moreInfo = document.getElementById('moreInfo');
+    moreInfo.classList.toggle('d-none'); // Toggle the visibility of the additional info
+});
